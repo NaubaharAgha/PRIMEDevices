f5315ee8e594498eff3ee4b10f4a6ec5f54c6f95
NaubaharAgha <naubahar@sragha.com>
2018-12-06 10:39:38 +0100

    First Trial of the BPRIME software
    First upload of trial software for running the BPRIME. Many things not working.
    TODO: Add a test mode which takes button input and rotates the barrel.

---
+++ BPRIME/BPrimeTest/BPrimeTest.h
@@ -0,0 +1,91 @@
+//---------------------------------- Experimental Parameters
+
+// DEBUG FLAG, set to 1 for Serial Output, 0 to mute output
+bool Debug = 1;
+
+// Running Mode:
+// S: "Standalone mode" doesn't require external input
+// M: "Moroco mode" Waits for Moroco input to progress through the trial
+// X: "XBI mode" communicates with the XBI <---- NOT IMPLEMENTED YET! ----
+char runningMode = 'M';
+
+// Trial Types:
+// "normal": cue stays on
+// "memory": cue turns off after "cueDisplayTime" milliseconds
+// "offset": device starts off center by "offsetAmount"
+String trialType = "normal";
+int offsetAmount = 30; // Set offset amount from 90 (i.e. 30 means 90 +- 30) 
+int cueDisplayTime = 1000; // Memory trial type, cue show time (in ms)
+
+int rotationSpeed = 1; // speed of barrel rotation
+int ITI = 1000; // Intertrial Interval in ms (time after resetting device and starting new trial)
+
+int timeToWaitAfterTrigger = 3000; // Wait time after the food well sensor is triggered (in ms)
+
+//---------------------------------- Serial Communication Protocol
+
+#define RESET '$'
+#define MANUAL_REWARD 'R'
+#define REWARD 'r'
+
+//---------------------------------- Pin Assignments 
+
+const byte interruptPin = 2;
+
+// Encoder and servo Pins
+int encoder0PinB = 3;
+int encoder0PinA = 7;
+
+int foodwellLEDsPIN = 4; //Foodwell LEDs
+int cueLEDsPIN = 5; //Signal light cue LEDs
+int servoPin = 6;
+
+// Need to remain constants for the rest of the code (switch statement)
+const int sensorLB = 8;
+const int sensorLT = 9;
+const int sensorRT = 10;
+const int sensorRB = 11;
+
+int boardLED = 13;
+int barrelrollLEDsPIN = 14; //Front face Left and Right indicators
+
+int startTrialTrigger = 15; //Input to trigger start of a trial
+
+const int stp 23
+const int dir 22
+const int MS1 21
+const int MS2 20
+const int AENABLE  19
+const int debug 18
+
+//---------------------------------- Initialization
+
+// Initialize Servo
+int startAngle = 90;
+Servo servo_0;
+int angle = startAngle;   // servo position in degrees 
+
+// Initialize LED strips
+Adafruit_NeoPixel cueStrip = Adafruit_NeoPixel(3, cueLEDsPIN);
+Adafruit_NeoPixel foodwellStrip = Adafruit_NeoPixel(4, foodwellLEDsPIN);
+Adafruit_NeoPixel barrelrollStrip = Adafruit_NeoPixel(2, barrelrollLEDsPIN);
+
+//---------------------------------- Variables
+
+// Placeholder variables for the encoder counting
+volatile unsigned int encoder0Pos = 65536/2; // Start encoder count from the middle
+volatile boolean PastA = 0;
+volatile boolean PastB = 0;
+
+bool keepRunning = false; // Boolean if the sensor -> motor loop should keep running or stop (this is set in the motor loop)
+
+// Colors declarations
+uint32_t off = cueStrip.Color(0, 0, 0);
+uint32_t white = cueStrip.Color(255, 255, 255);
+uint32_t red = cueStrip.Color(255, 0, 0);
+uint32_t green = cueStrip.Color(0, 255, 0);
+uint32_t blue = cueStrip.Color(0, 0, 255);
+uint32_t yellow = cueStrip.Color(255, 255, 0);
+uint32_t pink = cueStrip.Color(255, 0, 255);
+uint32_t orange = cueStrip.Color(255, 128, 0);
+uint32_t cyan = cueStrip.Color(0, 255, 255);
---
+++ BPRIME/BPrimeTest/BPrimeTest.ino
@@ -0,0 +1,295 @@
+#include <Servo.h>
+#include <Adafruit_NeoPixel.h>
+#include <Encoder.h>
+#include "SmartBarrelTest.h"
+
+void setup()
+{
+  // Set Encoder pins as input
+  pinMode(encoder0PinA, INPUT);
+  digitalWrite(encoder0PinA, HIGH);       // turn on pull-up resistor
+  pinMode(encoder0PinB, INPUT);
+  digitalWrite(encoder0PinB, HIGH);       // turn on pull-up resistor
+
+  // Initial value of encoder pins
+  PastA = (boolean)digitalRead(encoder0PinA); //initial value of channel A;
+  PastB = (boolean)digitalRead(encoder0PinB); //and channel B
+  
+  // Attach servo, initialize LED pin, and LCD outputs
+  servo_0.attach(servoPin);
+  servo_0.write(angle);  // Initialize to front center angle
+  delay(1000);
+  pinMode(boardLED, OUTPUT);
+
+  // Set IR sensor pins as input
+  pinMode(sensorRT, INPUT);
+  pinMode(sensorRB, INPUT);
+  pinMode(sensorLT, INPUT);
+  pinMode(sensorLB, INPUT);
+  
+  // Set IR inputs (all go into same pin) as an interrupt
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), sensorInterrupt, FALLING);
+
+  pinMode(startTrialTrigger, INPUT_PULLDOWN);
+  //digitalRead(startTrialTrigger);
+  
+  // Initialize Cue Light Strip
+  cueStrip.begin();
+  cueStrip.show(); // Initialize all pixels to 'off'
+  cueStrip.setBrightness(255);
+
+  // Initialize Cue Light Strip
+  barrelrollStrip.begin();
+  barrelrollStrip.setBrightness(255);
+  barrelrollStrip.setPixelColor(0, orange); // Left is Orange
+  barrelrollStrip.setPixelColor(1, cyan);   // Right is Cyan
+  barrelrollStrip.show();
+  
+  // Initialize and light up food wells, declared here since these dont change
+  // 0 = Left Top (LT) = Green
+  // 1 = Left Bottom (LB) = Pink
+  // 2 = Right Bottom (RB) = Blue
+  // 3 = Right Top (RT) = Yellow
+  foodwellStrip.begin();
+  foodwellStrip.setBrightness(255);
+  foodwellStrip.setPixelColor(0, green);
+  foodwellStrip.setPixelColor(1, pink);
+  foodwellStrip.setPixelColor(2, blue);
+  foodwellStrip.setPixelColor(3, yellow);
+  foodwellStrip.show();
+
+  Serial.begin(9600);
+  Serial.print("Setup");
+  
+  resetDevice();
+  prepareTrial();
+  
+}
+
+void loop()
+{
+  switch(runningMode){
+  case 'S':
+    while(digitalRead(startTrialTrigger)){
+      if (Debug){
+        Serial.println(" ");
+        Serial.println("Triggered!");
+      }
+      startTrial(pickNewRandTarget());
+    }
+    break;
+  case 'M':
+    // Moroco wait for serial input
+
+    if (Serial.available() > 0)
+    {
+      c = Serial.read();
+    }
+  
+    //handle reset request
+    if (c == RESET)
+    {
+      if (Serial.read() == RESET)
+      {
+        if (Serial.read() == RESET)
+        {
+          reset = true;
+        }
+      }
+    }
+
+    switch c{
+      case REWARD:
+      
+      break;
+      case MANUAL_REWARD:
+      
+      break;
+      case NEW_TRIAL:
+      
+      break;
+      case SHOW_CUE:
+      
+      break;
+      case 'R':
+      
+      break;
+    }
+    
+    break;
+  }
+  
+// Reset the motor angle immediately, as soon as it reaches 0 or 180
+/*  
+  if((angle == 0) || (angle == 180)){
+    resetDevice();
+  }
+*/
+  if (Debug){
+    //Serial.print("In Loop");
+  }
+  
+}
+
+void rotateBarrel(int currTarget) {
+
+   if (Debug){
+      Serial.print("Ready to Rotate Barrel");
+      digitalWrite(boardLED, HIGH);
+   }
+
+   keepRunning = true;
+ // Keep allowing the barrel to move until the food well is accessed
+ while(keepRunning){
+
+  // Read from the encoder and determine which way it is turning
+  // then write that angle into servo, as long as it is between
+  // 0 and 180 degrees.
+  int aVal = digitalRead(encoder0PinA); // Read from Encoder Pin A
+   if (aVal != PastA) { // Means the knob is rotating
+    if (digitalRead(encoder0PinB) != aVal) { // Means pin A Changed first - We're Rotating Clockwise
+        encoder0Pos--;
+        if ((angle > 0) && (angle < 180)) {
+          angle += rotationSpeed;
+        }
+      } else {// Otherwise B changed first and we're moving CCW
+        encoder0Pos++;
+        if ((angle > 0) && (angle < 180)) {
+          angle -= rotationSpeed;
+        }
+        if ((angle == 180) || (angle == 0)) {
+          angle = 90;
+        }
+      }
+      servo_0.write(angle);
+      delay(15);  
+     
+   // DEBUG: Serial output of sensors and actuators
+    if (Debug){
+       Serial.print ("Angle: ");
+       Serial.print (angle);
+       Serial.print (", Enc: ");
+       Serial.print (encoder0Pos);
+       Serial.print (", RT:");
+       Serial.print (digitalRead(sensorRT));
+       Serial.print (", RB:");
+       Serial.print (digitalRead(sensorRB));
+       Serial.print (", LT:");
+       Serial.print (digitalRead(sensorLT));
+       Serial.print (", LB:");
+       Serial.println (digitalRead(sensorLB));
+    }
+  }
+ }
+}
+
+int pickNewRandTarget(){
+    if (Debug){
+      Serial.println("Picking a new Target");
+    }
+    int a[4] = {sensorRT,sensorRB,sensorLT,sensorLB};
+    // Pick current trial target
+    int currTarget = a[rand() % 3];
+
+    return currTarget;
+}
+
+void showCue(int targetID){
+ if (Debug){
+  Serial.println("Showing Cue");
+ }
+  // 0 = Left Top (LT) = Green
+  // 1 = Left Bottom (LB) = Pink
+  // 2 = Right Bottom (RB) = Blue
+  // 3 = Right Top (RT) = Yellow
+  switch(targetID) {
+    case sensorRT : 
+      cueStrip.setPixelColor(1, cyan);
+      cueStrip.setPixelColor(2, yellow);
+      break;      
+    case sensorRB : 
+      cueStrip.setPixelColor(1, cyan);
+      cueStrip.setPixelColor(2, blue);
+        break;
+    case sensorLT : 
+      cueStrip.setPixelColor(1, orange);
+      cueStrip.setPixelColor(2, green);
+        break;     
+    case sensorLB : 
+      cueStrip.setPixelColor(1, orange);
+      cueStrip.setPixelColor(2, pink);
+        break;
+  }
+  cueStrip.show();
+  
+  if (trialType == "memory"){    
+  delay(cueDisplayTime);
+    cueStrip.setPixelColor(2, off);
+    cueStrip.show();
+  }
+  
+}
+
+void prepareTrial() {
+
+  // Begin Cue Strip ("start" mode)
+  cueStrip.setPixelColor(0, white);
+  cueStrip.setPixelColor(1, off);
+  cueStrip.setPixelColor(2, off);
+  cueStrip.show();
+  if(Debug){
+    Serial.println ("Device Ready");
+  }
+  delay(ITI);
+  
+  //digitalRead(startTrialTrigger)
+}
+
+void startTrial(int currTarget){
+  showCue(currTarget);
+  rotateBarrel(currTarget);
+}
+
+void resetDevice() {
+
+  // Turn off Cue Strip ("off" mode)
+  cueStrip.setPixelColor(0, off);
+  cueStrip.setPixelColor(1, off);
+  cueStrip.setPixelColor(2, off);
+  cueStrip.show();
+
+  if(Debug){
+    // Reset servo position
+    Serial.println ("Resetting Device...");
+  }
+  //delay(1000);
+  if (trialType == "offset"){ 
+    angle = 90 + offsetAmount;
+  } else {
+    angle = 90; 
+  }
+  servo_0.write(angle);
+  delay(1000);
+}
+
+void sensorInterrupt() {
+     // DEBUG: Serial output if IR sensor tripped
+  if (Debug){
+    Serial.println ("IR sensor tripped!");
+    digitalWrite(boardLED, LOW);
+  }
+  delay(timeToWaitAfterTrigger);
+  keepRunning = false;
+  servo_0.write(startAngle);
+  resetDevice();
+  if (Debug){
+    Serial.println ("Waiting for ready signal.");
+  }
+}
+
+void spinMotor() {
+  
+}
+}
+
